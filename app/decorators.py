f93042a1944fe58a972f1ef86bc9e0f64fc10a2f
marthamareal <marthamareal@gmail.com>
2018-07-04 14:06:02 +0300

    Add CI and coverage

--- app/decorators.py
+++ app/decorators.py
@@ -11,13 +11,14 @@ def login_required(decorated_function):
     @wraps(decorated_function)
     def wrapper_func(*args, **kwargs):
         token = request.headers.get("token")
-        user_id = request.headers.get("user_id")
+
         if not token:
             return jsonify({"Error": "Please supply the token in headers"})
 
         details = jwt.decode(token, secret, algorithms='HS256', verify=False)
+        print(details)
 
-        return decorated_function(details.get(user_id, token,  *args, **kwargs))
+        return decorated_function()
 
     return wrapper_func
 
