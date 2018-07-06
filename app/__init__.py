01307945e8753851fbf449747cf9dded084c5a9f
marthamareal <marthamareal@gmail.com>
2018-07-06 11:39:26 +0300

    add notifications

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flasgger import Swagger
 from flask import Flask, make_response, jsonify, redirect
 
 from app.db_manager import DatabaseManager
+from app.notifications.views import blue_print_notifications
 from app.requests.views import blue_print_requests
 from app.rides.views import blue_print_rides
 from app.user.views import blue_print_user
@@ -24,6 +25,7 @@ Swagger(app, template=template)
 app.register_blueprint(blue_print_user)
 app.register_blueprint(blue_print_rides)
 app.register_blueprint(blue_print_requests)
+app.register_blueprint(blue_print_notifications)
 
 
 @app.route('/')
