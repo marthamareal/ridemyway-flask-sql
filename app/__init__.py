1f9c3bc75d8e784672abe9361ae7449f6fc886f2
marthamareal <marthamareal@gmail.com>
2018-07-25 11:24:27 +0300

    add COR for resource sharing

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flasgger import Swagger
 from flask import Flask, make_response, jsonify, redirect
+from flask_cors import CORS
 
 from app.db_manager import DatabaseManager
 from app.requests.views import blue_print_requests
@@ -7,6 +8,7 @@ from app.rides.views import blue_print_rides
 from app.user.views import blue_print_user
 
 app = Flask(__name__)
+CORS(app)
 
 # creating my swagger ui info
 
